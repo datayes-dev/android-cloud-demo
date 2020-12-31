6592ea2c4aaf7053287c87525f66c564759a730d
高神恩 <shenen.gao@datayes.com>
2020-12-31 14:08:17 +0800

    通联登陆demo提交

---
+++ app/src/main/java/com/datayes/clouddemo/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.datayes.clouddemo
+
+import android.os.Bundle
+import com.datayes.iia.module_common.base.BaseActivity
+
+class MainActivity : BaseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun getContentLayoutRes() = R.layout.activity_main
+
+}
