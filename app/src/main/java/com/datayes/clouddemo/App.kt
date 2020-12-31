7b51e4b41eed54fdcad1a27badb9a24150aee21e
高神恩 <shenen.gao@datayes.com>
2020-12-31 15:54:29 +0800

    修改demo

---
+++ app/src/main/java/com/datayes/clouddemo/App.kt
@@ -0,0 +1,26 @@
+package com.datayes.clouddemo
+
+import android.app.Application
+import com.datayes.common.net.Environment
+import com.datayes.iia.module_common.base.x5webview.X5WebViewManager
+import com.datayes.rrp.cloud.DataYesCloud
+
+class App : Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+        // 初始化通联数据环境
+        DataYesCloud.INSTANCE.init(
+            this,
+            Environment.STG,
+            "9",  // 通联数据产品Id，需要通联云平台做配置
+            "xiaomi",
+            BuildConfig.DEBUG
+        )
+
+        // 初始化一键登录
+        DataYesCloud.INSTANCE.initOneBtnLogin(this, BuildConfig.DEBUG)
+        // 初始化x5 webView自动带有登录信息【非必须】
+        X5WebViewManager.INSTANCE.initX5()
+    }
+}
--- app/src/main/java/com/datayes/clouddemo/MainActivity.kt
+++ app/src/main/java/com/datayes/clouddemo/MainActivity.kt
@@ -1,14 +1,60 @@
 package com.datayes.clouddemo
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import com.alibaba.android.arouter.launcher.ARouter
+import com.datayes.common_cloud.user.User
+import com.datayes.common_cloud.user.UserManager
 import com.datayes.iia.module_common.base.BaseActivity
+import com.datayes.irr.rrp_api.ARouterPath
+import com.datayes.irr.rrp_api.RrpApiRouter
+import com.datayes.rrp.cloud.RouterPath
 
 class MainActivity : BaseActivity() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    override fun getContentLayoutRes() = R.layout.activity_main
+
+    fun onLoginClick(view: View?) {
+        if (User.INSTANCE.isLogin) {
+            Toast.makeText(this, "已登录", Toast.LENGTH_SHORT).show()
+        } else {
+            ARouter.getInstance().build(RrpApiRouter.LOGIN_PAGE).navigation()
+        }
     }
 
-    override fun getContentLayoutRes() = R.layout.activity_main
+    fun onLogoutClick(view: View?) {
+        // 登出
+        UserManager.INSTANCE.loginOut()
+        Toast.makeText(this, "登出成功", Toast.LENGTH_SHORT).show()
+    }
+
+    fun onOneBtnLoginClick(view: View?) {
+        if (User.INSTANCE.isLogin) {
+            Toast.makeText(this, "已登录", Toast.LENGTH_SHORT).show()
+        } else {
+            ARouter.getInstance().build(RrpApiRouter.LOGIN_PAGE)
+                .withBoolean("useDialog", true)
+                .navigation()
+        }
+    }
+
+    fun showUserInfo(view: View?) {
+        if (User.INSTANCE.isLogin) {
+            ARouter.getInstance().build(ARouterPath.USER_INFO_V2_PAGE)
+                .navigation()
+        }
+    }
+
+    fun onBindClick(v: View) {
+        if (User.INSTANCE.isLogin) {
+            ARouter.getInstance().build(RouterPath.ACCOUNT_SECURITY)
+                .navigation()
+        }
+    }
+
+    fun onRequestDemo(v: View) {
+
+    }
 
 }
