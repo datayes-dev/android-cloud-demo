91f2b00c12deba591a7f45bf9ead9fa8ec435ac2
高神恩 <shenen.gao@datayes.com>
2021-01-13 18:07:45 +0800

    修复图片验证码不显示的bug

---
+++ app/src/main/java/com/datayes/clouddemo/AppGlideModule.java
@@ -0,0 +1,14 @@
+package com.datayes.clouddemo;
+
+import com.bumptech.glide.annotation.GlideModule;
+import com.datayes.common_imageloader.module.MyGlideModule;
+
+/**
+ * GlideModule
+ *
+ * @author shenen.gao
+ */
+@GlideModule
+public class AppGlideModule extends MyGlideModule {
+
+}
